c6ea803e468ed71ab8fade27584f6f50382e5030
Sanderson Srisouraj <ssanderson295@gmail.com>
2022-03-22 23:07:56 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const letter = []
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        letter.push(message)
+    }
+    return letter
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+
+function countDown() {
+    let countDown = 0
+    while (countDown <= 10) {
+       console.log(countDown);
+       countDown++;
+    }
+    return;
+}
+
